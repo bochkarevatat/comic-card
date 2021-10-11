f1c9e273877fccbf6c1d4e9bfdbcf6f3eb407a0f
bochkarevatat <tanitabt@rambler.ru>
2021-10-11 17:41:36 +0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+    <div id="app">
+
+        <app-header/>
+
+        <div class="container">
+            <h1 class="pt-3 pb-3">Персонажи Marvel</h1>
+        <!-- <pre>characters:{{characters}}</pre> -->
+            <app-modal :character="characters[characterIndex]"/>
+
+            <spinner v-if="loading"/>
+
+            <div class="row">
+                 
+                 <div v-for="(el, idx) in characters" class="card mb-3" style="max-width: 340px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img :src=el.thumbnail class="img-fluid rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{el.name}}</h5>
+          <p class="card-text"></p>
+          <p class="card-text"><small class="text-muted"></small></p>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal" @click="characterIndex = idx">
+            Узнать больше!
+          </button>
+
+        </div>
+      </div>
+    </div>
+  </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import Spinner from "./components/Spinner";
+    import AppModal from "./components/AppModal";
+    import AppHeader from "./components/AppHeader";
+
+    export default {
+        name: 'App',
+        components: {
+            AppHeader,
+            AppModal,
+            Spinner,
+        },
+        data() {
+            return {
+                loading: false,
+                characters: [],
+                characterIndex: 0,
+            }
+        },
+        methods: {
+
+            fetchCharacters: function (){
+                return fetch('https://netology-api-marvel.herokuapp.com/characters')
+                    .then(res => res.json())
+                    .then(json => this.characters = json)
+                
+            },
+        },
+        computed: {},
+        mounted(){
+            this.loading = true
+            this.fetchCharacters()
+            this.loading = false
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand">Персонажи Marvel</a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light" type="submit">все</button>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        data() {
+            return {
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
